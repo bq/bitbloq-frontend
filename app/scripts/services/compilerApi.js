022d83bfd6f600a89b345baa94bf2d362ed1d3cb
David Garcia <empoalp@gmail.com>
2018-06-28 10:01:55 +0200

    Remote compilation using new web2board

--- app/scripts/services/compilerApi.js
+++ app/scripts/services/compilerApi.js
@@ -8,19 +8,71 @@
  * Service in the bitbloqApp.
  */
 angular.module('bitbloqApp')
-    .service('compilerApi', function(envData, $http) {
+    .service('compilerApi', function(envData, $q, $websocket, WSHubsAPI) {
         var exports = {
             compile: compile
         };
 
-        function compile(data) {
+        var api,
+            host = envData.config.remoteWeb2boardHost,
+            port = envData.config.remoteWeb2boardPort,
+            timeoutResponse = 45000; //45 seconds
 
-            return $http({
-                method: 'POST',
-                url: envData.config.compilerUrl + 'compile',
-                data: data
+        function webSocketWrapper(url) {
+            var ws = $websocket(url);
+            ws.onMessage(function (ev) {
+                ws.onmessage(ev);
+            });
+            ws.onOpen(function (ev) {
+                ws.onopen(ev);
+            });
+            ws.onClose(function (ev) {
+                ws.onclose(ev);
+            });
+            return ws;
+        }
+
+        function isWSConnected(wsClient) {
+            return wsClient && (wsClient.readyState === WebSocket.CONNECTING || wsClient.readyState === WebSocket.OPEN);
+        }
+
+        function connect() {
+            return $q(function(resolve, reject) {
+                try {
+                    if (isWSConnected(api.wsClient)) {
+                        resolve();
+                    } else {
+                        api.connect('wss://' + host + ':' + port + '/bitbloq').then(resolve, reject);
+                    }
+                } catch (e) {
+                    reject(e);
+                }
             });
         }
+
+        function compile(data) {
+            return $q(function(resolve, reject) {
+                connect().then(
+                    function() {
+                        var board = data.board;
+                        var code = data.code;
+                        return api.CodeHub.server.compile(code,(board && board.mcu) || 'uno').then(
+                            function(hex) {
+                                resolve({ data: { hex: hex }});
+                            },
+                            function(error) {
+                                resolve({ data: { error: error }});
+                            }
+                        );
+                    },
+                    function() {
+                        reject({ data: 'remote-web2board-connection-error' });
+                    }
+                );
+            })
+        }
+
+        api = WSHubsAPI.construct(timeoutResponse, webSocketWrapper, $q);
 
         return exports;
     });
